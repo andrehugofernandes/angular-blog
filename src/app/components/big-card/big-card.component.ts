60a53669b840f43c09b2e0a420962b95abae73cd
andrehugofernandes <andrehugofernandes@gmail.com>
2022-12-08 08:13:50 -0300

    Content page created. Project finished

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,11 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
   @Input()
-  photoCover:string = "";
+  photoCover:string = ""
   @Input()
-  cardTitle:string = "";
+  cardTitle:string = ""
   @Input()
-  cardDescription:string = "";
+  cardDescription:string = ""
+  @Input()
+  Id:string = "0"
 
   constructor(){}
   ngOnInit(): void {}
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Dear UI Designer, yok bisa yok belajar bikin copy sendiri",
+    "description":"Lupakan sejenak Lorem Ipsum. Buat copy mu sendiri. Buat rekan kerja UX Writer lebih happy saat bekerja",
+    "photoCover":"https://www.dwinawan.com/blog/thumb_article35.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Ketika kamu bingung mengisi konten pada eksplorasi design mu",
+    "description":"Pernahkah kamu saat membuat design sebuah halaman lantas bingung halaman ini sebaiknya diisi apa ya? konten atau elemen apa yang cocok untuk halaman ini?",
+    "photoCover":"https://www.dwinawan.com/blog/thumb_article34.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Berlatih UX Writing dengan Prinsip Dasar Microcopy",
+    "description":"UX Writing atau Copy pada UI Design cukup berpengaruh atas nyaman atau tidaknya suatu produk untuk digunakan. Makin ringkas dan jelas suatu copy akan membantu user untuk mudah mengerti. Bagaimana untuk melatih kemampuan membuat tulisan atau copy yang mudah dimengerti? Bisa dengan menggunakan 4 prinisp microcopy yaitu Clear, Concise, Consistent dan Functional.",
+    "photoCover":"https://www.dwinawan.com/blog/thumb_article15.jpg"
+  },
+  {
+    "id":"4",
+    "title":"Cara cepat belajar design landing page",
+    "description":"Luangkan waktu 20 detik untuk membaca artikel ini dan kalian akan mengetahui langkah efektif metode ATM (Amati, Tiru dan Modifikasi) dalam proses belajar design landing page",
+    "photoCover":"https://www.dwinawan.com/blog/thumb_article11.jpg"
+  }
+]
